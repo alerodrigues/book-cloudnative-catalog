fcb07d32b8ebb0c820e8727c789dab1efb7ba579
alerodrigues <ale.rodrigues@live.com>
2026-01-20 22:44:18 +0000

    first commit

---
+++ src/main/java/com/example/book_cloud_native/BookCloudNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_cloud_native;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCloudNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCloudNativeApplication.class, args);
+	}
+
+}
